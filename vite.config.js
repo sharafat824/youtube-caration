4799fa28db10eb54864a89cb9196df37c25ff0ae
Ali <sharaft.codes@gmail.com>
2026-01-15 00:33:05 +0500

    minor update loaded env through loadenv mothod

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+const env = loadEnv(mode, process.cwd(), '')
+
 export default defineConfig({
   plugins: [
     vue(),
