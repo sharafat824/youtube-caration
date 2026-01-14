01588735676d3b554322f3a05aee825d21a174b3
Ali <sharaft.codes@gmail.com>
2026-01-15 00:34:52 +0500

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,38 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-const env = loadEnv(mode, process.cwd(), '')
+export default defineConfig(({ mode }) => {
+  // Load environment variables
+  const env = loadEnv(mode, process.cwd(), '')
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: env.VITE_API_URL,
-        changeOrigin: true,
-        secure: false,
-      },
-      '/sanctum': {
-        target: env.VITE_API_URL,
-        changeOrigin: true,
-        secure: false,
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+      tailwindcss(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       },
     },
-  },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL, // now works
+          changeOrigin: true,
+          secure: false,
+        },
+        '/sanctum': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  }
 })
